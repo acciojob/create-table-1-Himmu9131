b2d821859b70855dd5a7c118c49c0e283db94cc5
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-07 23:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
+	const table=document.getElementById("sampleTable");
+	const newRow = table.insertRow(0); 
+	const firstCell=newRow.insertCell(0);
+	const secondCell=newRow.insertCell(1);
+	firstCell.textContent='New cell1';
+	secondCell.textContent='New cell2';
+	
   
   
 }
