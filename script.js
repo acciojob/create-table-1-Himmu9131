60b868c289e78810c0bd5f9f239a37d7e94fe538
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-08 00:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	const newRow = table.insertRow(0); 
 	const firstCell=newRow.insertCell(0);
 	const secondCell=newRow.insertCell(1);
-	firstCell.textContent='New cell1';
-	secondCell.textContent='New cell2';
+	firstCell.textContent='New Cell1';
+	secondCell.textContent='New Cell2';
 	
   
   
